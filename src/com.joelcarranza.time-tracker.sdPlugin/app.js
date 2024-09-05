34c592851f5fd7f814d932f1731da3be56e345c8
Joel Carranza <jec@gatekeeper.com>
2024-09-05 13:51:30 -0500

    prompt for API key and query toggl

--- src/com.joelcarranza.time-tracker.sdPlugin/app.js
+++ src/com.joelcarranza.time-tracker.sdPlugin/app.js
@@ -1,19 +1,66 @@
 /// <reference path="libs/js/action.js" />
 /// <reference path="libs/js/stream-deck.js" />
 
-const myAction = new Action('com.elgato.template.action');
+const myAction = new Action('com.joelcarranza.time-tracker.action');
 
-/**
- * The first event fired when Stream Deck starts
- */
+
 $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
 	console.log('Stream Deck connected!');
 });
 
-myAction.onKeyUp(({ action, context, device, event, payload }) => {
-	console.log('Your key code goes here!');
+myAction.onWillAppear(({ action, context, device, event, payload }) =>  {
+	let settings = payload.settings;
+	this.apitoken = settings['apitoken']
+
+	if(this.apitoken) {
+		togglGetCurrentEntry(this.apitoken).then(response => {
+			if (response.ok) {
+				response.json().then(responseData => {
+					console.log("RESULT!");
+					console.log(responseData);
+			});
+			}
+			else {
+				console.log("NOPE!");
+				console.log(response);
+			}
+		});
+	}
 });
 
-myAction.onDialRotate(({ action, context, device, event, payload }) => {
-	console.log('Your dial code goes here!');
+
+myAction.onDidReceiveSettings(({ action, context, device, event, payload }) =>  {
+	let settings = payload.settings;
+	this.apitoken = settings['apitoken']
+
+	if(this.apitoken) {
+		togglGetCurrentEntry(this.apitoken).then(response => {
+			if (response.ok) {
+				response.json().then(responseData => {
+					console.log(responseData);
+			});
+			}
+			else {
+				console.log("NOPE!");
+				console.log(response);
+			}
+		});
+	}
 });
+
+myAction.onKeyDown(({ action, context, device, event, payload }) => {
+	console.log("onKeyDown " + this.apitoken);
+});
+
+
+const togglBaseUrl = 'https://api.track.toggl.com/api/v9';
+
+function togglGetCurrentEntry(apiToken) {
+	return fetch(
+	  `${togglBaseUrl}/me/time_entries/current`, {
+	  method: 'GET',
+	  headers: {
+		Authorization: `Basic ${btoa(`${apiToken}:api_token`)}`
+	  }
+	});
+}
